943165fac8219c1bab8bbf0e854dfb2b913d431f
Lilith Wittmann <mail@lilithwittmann.de>
2024-04-21 20:25:19 +0200

    move graph node to components

---
+++ src/components/graph/GraphNode.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import {Handle, Position} from "@vue-flow/core";
+export default {
+  name: "Node",
+  components: {Handle},
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+  },
+  computed: {
+    Position() {
+      return Position
+    }
+  }
+}
+</script>
+
+<template>
+  <Handle id="top" :position="Position.Top"/>
+  <Handle id="left" :position="Position.Left" />
+  <div class="node">
+    {{ label }}
+  </div>
+  <Handle id="right" :position="Position.Right"/>
+  <Handle id="bottom" :position="Position.Bottom" />
+</template>
+
+<style scoped>
+.node {
+  background-color: #fff;
+  min-width: 150px;
+  max-width: 250px;
+  border-radius: 4px;
+  text-align: center; color: #000;
+  padding: 15px 10px;
+}
+</style>
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Button from './components/button/Button.vue';
+import GraphNode from './components/graph/GraphNode.vue';
 import './assets/generated/variables.css';
 // Export the Button component
-export { Button };
+export { Button, GraphNode };
